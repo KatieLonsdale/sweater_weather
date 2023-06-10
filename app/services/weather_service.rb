bf78f9c724f014327c57d563cc844ff7f1fcb593
Katie Lonsdale <katie952@gmail.com>
2023-06-10 17:53:49 -0600

    feat: add hourly_weather method

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -14,6 +14,10 @@ class WeatherService
     get_url ("/v1/forecast.json?days=5&q=#{@lat},#{@lon}")
   end
 
+  def hourly_weather
+    get_url ("/v1/forecast.json?q=#{@lat},#{@lon}")
+  end
+
   def conn
     Faraday.new(url: 'http://api.weatherapi.com') do |site|
       site.params['key'] = ENV['WEATHER_API_KEY']
