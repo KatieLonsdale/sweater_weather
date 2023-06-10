c84c3830e2b4000415a7d93e41af609a7f877ebf
Katie Lonsdale <katie952@gmail.com>
2023-06-10 16:19:11 -0600

    refactor: change methods to instance methods

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe WeatherService do
   describe 'class methods' do
-    describe '::current_weather' do
+    describe '::initialize' do
+      it 'exists and has attributes' do
+        cw = WeatherService.new(38.89037, 77.03196)
+        expect(cw).to be_a(WeatherService)
+        expect(cw.lat).to eq(38.89037)
+        expect(cw.lon).to eq(77.03196)
+      end
+    end
+  end
+  describe 'instance methods' do
+    describe '#current_weather' do
       it 'returns the weather api response for current weather for given lat/lon' do
         keys = [:last_updated, :temp_f, :feelslike_f, :humidity, :uv, :vis_miles, :condition]
-        lat = 38.89037
-        lon = -77.03196
-        cw = WeatherService.current_weather(lat, lon)
+        cw = WeatherService.new(38.89037, -77.03196).current_weather
 
         expect(cw).to have_key(:current)
         keys.each do |key|
@@ -17,11 +25,9 @@ RSpec.describe WeatherService do
         expect(cw.dig(:current, :condition)).to have_key(:icon)
       end
     end
-    describe '::daily_weather' do
+    describe '#daily_weather' do
       it 'returns the weather api response for 5 days weather for given lat/lon' do
-        lat = 38.89037
-        lon = -77.03196
-        dw = WeatherService.daily_weather(lat, lon)
+        dw = WeatherService.new(38.89037, -77.03196).daily_weather
 
         expect(dw.dig(:forecast, :forecastday, 0)).to have_key(:date)
         expect(dw.dig(:forecast, :forecastday, 0)).to have_key(:day)
@@ -35,7 +41,7 @@ RSpec.describe WeatherService do
         expect(dw.dig(:forecast, :forecastday, 0, :astro)).to have_key(:sunset)
       end
     end
-    describe '::hourly_weather' do
+    describe '#hourly_weather' do
       xit 'returns the weather api response for hourly weather for given lat/lon' do
 
       end
