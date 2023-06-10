cc6ac0cb39067fcdfca5f9370a52d3dd60abe54d
Katie Lonsdale <katie952@gmail.com>
2023-06-10 16:19:26 -0600

    refactor: add initialize and change methods to instance methods

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,19 +1,26 @@
 class WeatherService
-  def self.current_weather(lat, lon)
-    get_url("/v1/current.json?q=#{lat},#{lon}")
+  attr_reader :lat, :lon
+
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
   end
 
-  def self.daily_weather(lat, lon)
-    get_url ("/v1/forecast.json?days=5&q=#{lat},#{lon}")
+  def current_weather
+    get_url("/v1/current.json?q=#{@lat},#{@lon}")
   end
 
-  def self.conn
+  def daily_weather
+    get_url ("/v1/forecast.json?days=5&q=#{@lat},#{@lon}")
+  end
+
+  def conn
     Faraday.new(url: 'http://api.weatherapi.com') do |site|
       site.params['key'] = ENV['WEATHER_API_KEY']
     end
   end
 
-  def self.get_url(url)
+  def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
