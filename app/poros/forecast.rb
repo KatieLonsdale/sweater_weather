873e97e45a18caab63324cb2dc65d3bfe4fa6d04
Katie Lonsdale <katie952@gmail.com>
2023-06-11 15:05:53 -0600

    feat: add id attribute

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,7 +1,8 @@
 class Forecast
-  attr_reader :current_weather, :daily_weather, :hourly_weather
+  attr_reader :id, :current_weather, :daily_weather, :hourly_weather
 
   def initialize(cw, dw, hw)
+    @id = nil
     @current_weather = cw
     @daily_weather = dw
     @hourly_weather = hw
