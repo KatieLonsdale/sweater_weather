341d5ec380f1abd0137384af27c7acd8dd2f6798
Katie Lonsdale <katie952@gmail.com>
2023-06-10 19:55:55 -0600

    test: change test to expect objects instead of hashes

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,12 +1,16 @@
-require 'rails_helper' do
+require 'rails_helper'
+
+RSpec.describe ForecastFacade do
   describe 'class methods' do
     describe '::weather_for_city' do
-      it 'returns a forecast object with weather attributes' do
+      it 'returns a forecast object with weather attributes', :vcr do
         forecast = ForecastFacade.weather_for_city('new york, ny')
         expect(forecast).to be_a(Forecast)
-        expect(forecast.current_weather).to be_a(Hash)
-        expect(forecast.daily_weather).to be_a(Hash)
-        expect(forecast.hourly_weather).to be_a(Hash)
+        expect(forecast.current_weather).to be_a(CurrentWeather)
+        expect(forecast.daily_weather).to be_a(Array)
+        expect(forecast.daily_weather[0]).to be_a(DailyWeather)
+        expect(forecast.hourly_weather).to be_a(Array)
+        expect(forecast.hourly_weather[0]).to be_a(HourlyWeather)
       end
     end
   end
