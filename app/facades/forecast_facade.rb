76ede3ad4e5e670086098b8cda89ebe89f93d1a6
Katie Lonsdale <katie952@gmail.com>
2023-06-10 19:57:58 -0600

    feat: add weather_for_city method

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,22 @@
+class ForecastFacade
+  def self.weather_for_city(location)
+    # look up lat/lon for given location
+    lat_lon = LocationService.location_info(location)
+    lat = lat_lon.dig(:results, 0, :locations, 0, :latLng, :lat)
+    lon = lat_lon.dig(:results, 0, :locations, 0, :latLng, :lng)
+    # send lat/lon to weather service(3)
+    cw = WeatherService.new(lat, lon).current_weather
+    dw = WeatherService.new(lat, lon).daily_weather
+    hw = WeatherService.new(lat, lon).hourly_weather
+    # create cw, hw, dw objects with weather service returns
+    cwo = CurrentWeather.new(cw[:current])
+    dwo = dw.dig(:forecast, :forecastday).map do |day|
+      DailyWeather.new(day)
+    end
+    hwo = hw.dig(:forecast, :forecastday, 0, :hour).map do |hour|
+      HourlyWeather.new(hour)
+    end
+    # create forecast object with cw, hw, dw as attributes
+    Forecast.new(cwo, dwo, hwo)
+  end
+end
