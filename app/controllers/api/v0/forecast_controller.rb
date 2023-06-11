bf28d382ea0e07ab423b3722c5cde60860ee1339
Katie Lonsdale <katie952@gmail.com>
2023-06-11 10:10:35 -0600

    feat: add serializer

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::ForecastController < ApplicationController
   def index
-    @forecast = ForecastFacade.new(params[:location]).weather_for_city
+    forecast = ForecastFacade.new(params[:location]).weather_for_city
+    render json: ForecastSerializer.new(forecast)
   end
 end
