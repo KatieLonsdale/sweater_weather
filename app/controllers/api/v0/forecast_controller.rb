ddbc688a11e23af793739529b5ab2d0c21e09535
Katie Lonsdale <katie952@gmail.com>
2023-06-11 11:15:58 -0600

    feat: add sad path

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,6 +1,16 @@
 class Api::V0::ForecastController < ApplicationController
   def index
-    forecast = ForecastFacade.new(params[:location]).weather_for_city
-    render json: ForecastSerializer.new(forecast)
+    if params[:location].present?
+      forecast = ForecastFacade.new(params[:location]).weather_for_city
+      render json: ForecastSerializer.new(forecast)
+    else
+      bad_request_error
+    end
+  end
+
+  private
+
+  def bad_request_error
+    render json: {errors: {detail: "Invalid search."}}, status: 400
   end
 end
