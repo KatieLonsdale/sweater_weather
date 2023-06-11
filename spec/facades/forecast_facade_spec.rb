3750587656c6bbe9b1e4b5f65c2ed2e060a31afa
Katie Lonsdale <katie952@gmail.com>
2023-06-11 08:52:39 -0600

    refactor: test to make sure that correct number of dw's and hw's are being added

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -19,8 +19,10 @@ RSpec.describe ForecastFacade do
         expect(forecast).to be_a(Forecast)
         expect(forecast.current_weather).to be_a(CurrentWeather)
         expect(forecast.daily_weather).to be_a(Array)
+        expect(forecast.daily_weather.count).to eq(5)
         expect(forecast.daily_weather[0]).to be_a(DailyWeather)
         expect(forecast.hourly_weather).to be_a(Array)
+        expect(forecast.hourly_weather.count).to eq(24)
         expect(forecast.hourly_weather[0]).to be_a(HourlyWeather)
       end
     end
