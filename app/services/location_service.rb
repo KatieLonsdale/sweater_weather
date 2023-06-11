ab19f3d810a6d14fb6a1364356cc73387c04c7bb
Katie Lonsdale <katie952@gmail.com>
2023-06-10 20:16:21 -0600

    feat: add initialize method

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,15 +1,21 @@
 class LocationService
-  def self.location_info(location)
-    get_url("/geocoding/v1/address?location=#{location}")
+  attr_reader :location
+  
+  def initialize(location)
+    @location = location
   end
 
-  def self.conn
+  def location_info
+    get_url("/geocoding/v1/address?location=#{@location}")
+  end
+
+  def conn
     Faraday.new(url: 'https://www.mapquestapi.com') do |site|
       site.params['key'] = ENV['MAPQUEST_API_KEY']
     end
   end
 
-  def self.get_url(url)
+  def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
