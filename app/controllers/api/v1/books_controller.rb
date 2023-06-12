bfe65a38db63790bef8dbf5dc78185608891a49e
Katie Lonsdale <katie952@gmail.com>
2023-06-12 11:23:51 -0600

    feat: add sad path for invalid quantity param

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,10 +2,20 @@ class Api::V1::BooksController < ApplicationController
 
   def search
     if params[:location].present?
-      books = BooksFacade.new(params[:location]).search_by_location(params[:quantity].to_i)
+      books = BooksFacade.new(params[:location], quantity).search_by_location
       render json: BooksSerializer.new(books)
     else
       render json: {error:{message: "Validation failed: Location must be provided"}}, status: 422
+    end
+  end
+
+  private
+
+  def quantity
+    if params[:quantity].present? && params[:quantity].to_i > 0
+      params[:quantity].to_i
+    else
+      3
     end
   end
 end
