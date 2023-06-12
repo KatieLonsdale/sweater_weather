7b912c329d4529b986cd2cf2af822e0a5b2b3f8c
Katie Lonsdale <katie952@gmail.com>
2023-06-12 09:35:26 -0600

    refactor: initialize test to expect formatted location

--- spec/facades/books_facade.rb
+++ spec/facades/books_facade.rb
@@ -6,7 +6,19 @@ RSpec.describe BooksFacade do
       it 'exists and has attributes' do
         book = BooksFacade.new('denver,co')
         expect(book).to be_a(BooksFacade)
-        expect(book.location).to eq('denver,co')
+        expect(book.location).to eq('denver%20co')
+      end
+    end
+  end
+  describe 'instance methods' do
+    describe '#search_by_location' do
+      it 'returns book object' do
+        bf = BooksFacade.new('denver,co')
+        books = bf.search_by_location(5)
+
+        expect(books).to be_a(Books)
+        expect(books.destination).to eq('denver,co')
+        expect(books.books.count).to eq(5)
       end
     end
   end
