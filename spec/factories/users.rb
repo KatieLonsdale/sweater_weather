1592863bd51ee6a3919ed4ecaf99f1d06d09504e
Katie Lonsdale <katie952@gmail.com>
2023-06-12 08:57:55 -0600

    wip: sad paths for user creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
-    api_key { "MyString" }
+    email {Faker::Internet.email}
+    password_digest {Faker::Internet.password}
   end
 end
