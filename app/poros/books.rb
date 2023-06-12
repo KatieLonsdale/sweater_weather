8b1c3145a0dbaaae50f50378c082546106c65a87
Katie Lonsdale <katie952@gmail.com>
2023-06-12 10:50:33 -0600

    feat: add id attribute

--- app/poros/books.rb
+++ app/poros/books.rb
@@ -1,7 +1,8 @@
 class Books
-  attr_reader :destination, :forecast, :total_books_found, :books
+  attr_reader :id, :destination, :forecast, :total_books_found, :books
 
   def initialize(data)
+    @id = nil
     @destination = data[:destination]
     @forecast = data[:forecast]
     @total_books_found = data[:total_books_found]
