9648540fb7392048b49ac98a2d3032b704ccb840
Katie Lonsdale <katie952@gmail.com>
2023-06-12 15:18:16 -0600

    feat: add record not found error and message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
-rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
-
-  def render_record_invalid_response(error)
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+  
+  def render_record_invalid(error)
     render json: {error: {message: error.message}}, status: 422
+  end
+
+  def render_record_not_found(error)
+    render json: {error: {message: error.message}}, status: 404
   end
 end
