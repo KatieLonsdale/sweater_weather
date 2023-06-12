52cc8ecf1022afab87f99988584a2f57a50f64b9
Katie Lonsdale <katie952@gmail.com>
2023-06-12 15:26:34 -0600

    refactor: remove unneccessary method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::API
 rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
-rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
   
   def render_record_invalid(error)
     render json: {error: {message: error.message}}, status: 422
-  end
-
-  def render_record_not_found(error)
-    render json: {error: {message: error.message}}, status: 404
   end
 end
