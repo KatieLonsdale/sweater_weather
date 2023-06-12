af718b6b3568ace4a615cfcaad3aff46ca352c42
Katie Lonsdale <katie952@gmail.com>
2023-06-12 11:09:57 -0600

    refactor: comment out incomplete work from other branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
 rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
 
-def render_no_record_response
-  require 'pry'; binding.pry
-  render json: ErrorSerializer.error_message(error.message), status: :not_found
-end
+# def render_no_record_response
+#   require 'pry'; binding.pry
+#   render json: ErrorSerializer.error_message(error.message), status: :not_found
+# end
 end
