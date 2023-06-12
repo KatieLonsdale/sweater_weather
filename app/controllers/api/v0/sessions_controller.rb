aa0b319b0633d9b95ea7915943b1d21ad41e6cbc
Katie Lonsdale <katie952@gmail.com>
2023-06-12 15:17:55 -0600

    feat: add sad paths for session create

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,6 +1,18 @@
 class Api::V0::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    render json: UserSerializer.new(user)
+    if !user
+      raise ActiveRecord::RecordNotFound, "User not found."
+    elsif user.authenticate(params[:password])
+      render json: UserSerializer.new(user)
+    else
+      invalid_user
+    end
+  end
+
+  private
+
+  def invalid_user
+    render json: {error: {message: "Incorrect email or password."}}, status: 401
   end
 end
