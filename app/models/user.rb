11549eff08b547649ebed96684e958e793404212
Katie Lonsdale <katie952@gmail.com>
2023-06-11 20:21:28 -0600

    feat: add validations and creation of api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+  before_validation :create_api_key
+
   validates :email, 
             presence: true, 
             uniqueness: { case_sensitive: false}
+  validates :api_key, 
+            presence: true, 
+            uniqueness: true
   validates_presence_of :password
 
   has_secure_password
+
+  private
+
+  def create_api_key
+    SecureRandom.base64
+  end
 end
