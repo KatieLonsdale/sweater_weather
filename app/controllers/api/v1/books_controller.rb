a389070a2e56350dbc5409e2c2a99c2285bdc77b
Katie Lonsdale <katie952@gmail.com>
2023-06-12 10:50:17 -0600

    feat: add json renders to search action

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BooksController < ApplicationController
   def search
-    books = BooksFacade.new(params[:location].search_by_location(params[:quantity]))
+    books = BooksFacade.new(params[:location]).search_by_location(params[:quantity].to_i)
+    render json: BooksSerializer.new(books)
   end
 end
