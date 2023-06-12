2293f37b9007134891c91c5f20156e688f4ae3be
Katie Lonsdale <katie952@gmail.com>
2023-06-12 11:09:37 -0600

    feat: add sad path for no location

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::BooksController < ApplicationController
+
   def search
-    books = BooksFacade.new(params[:location]).search_by_location(params[:quantity].to_i)
-    render json: BooksSerializer.new(books)
+    if params[:location].present?
+      books = BooksFacade.new(params[:location]).search_by_location(params[:quantity].to_i)
+      render json: BooksSerializer.new(books)
+    else
+      render json: {error:{message: "Validation failed: Location must be provided"}}, status: 422
+    end
   end
 end
