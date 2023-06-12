84a183f8db7a09631519eb26c310e475a9e9ad3b
Katie Lonsdale <katie952@gmail.com>
2023-06-12 10:36:20 -0600

    test: add search by location test

--- spec/facades/books_facade.rb
+++ spec/facades/books_facade.rb
@@ -6,17 +6,18 @@ RSpec.describe BooksFacade do
       it 'exists and has attributes' do
         book = BooksFacade.new('denver,co')
         expect(book).to be_a(BooksFacade)
-        expect(book.location).to eq('denver%20co')
+        expect(book.location).to eq('denver,co')
       end
     end
   end
   describe 'instance methods' do
     describe '#search_by_location' do
-      it 'returns book object' do
+      it 'returns book object', :vcr do
         bf = BooksFacade.new('denver,co')
         books = bf.search_by_location(5)
 
         expect(books).to be_a(Books)
+        expect(books.forecast).to be_a(BriefForecast)
         expect(books.destination).to eq('denver,co')
         expect(books.books.count).to eq(5)
       end
