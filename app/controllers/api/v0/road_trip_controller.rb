33ba200e3b2171d59ae91f4475bda29be93d3011
Katie Lonsdale <katie952@gmail.com>
2023-06-13 11:05:11 -0600

    feat: add json rendering

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::RoadTripController < ApplicationController
   def create
-    require 'pry'; binding.pry
     road_trip = RoadTripFacade.new(road_trip_params).create_road_trip
+    render json: RoadTripSerializer.new(road_trip)
   end
 
   private
