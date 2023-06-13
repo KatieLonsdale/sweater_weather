80d134cb56e770c456bafb7456ca9604bbe4ec3d
Katie Lonsdale <katie952@gmail.com>
2023-06-13 09:24:57 -0600

    fix: method name

--- spec/services/road_trip_service_spec.rb
+++ spec/services/road_trip_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RoadTripService do
   describe 'instance methods' do
     describe 'get_road_trip' do
       it 'returns MapQuest road trip info', :vcr do
-        road_trip = RoadTripService.new.road_trip("Cincinatti,OH", "Chicago,IL")
+        road_trip = RoadTripService.new.get_road_trip("Cincinatti,OH", "Chicago,IL")
 
         expect(road_trip[:route]).to have_key(:formattedTime)
       end
