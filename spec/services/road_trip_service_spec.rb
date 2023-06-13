3b130e007aa96f1525dd1e01496b6fb00b7d6c79
Katie Lonsdale <katie952@gmail.com>
2023-06-13 09:20:11 -0600

    test: add get_road_trip test

--- spec/services/road_trip_service_spec.rb
+++ spec/services/road_trip_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RoadTripService do
   describe 'instance methods' do
     describe 'get_road_trip' do
-      it 'returns MapQuest road trip info' do
+      it 'returns MapQuest road trip info', :vcr do
         road_trip = RoadTripService.new.road_trip("Cincinatti,OH", "Chicago,IL")
 
         expect(road_trip[:route]).to have_key(:formattedTime)
