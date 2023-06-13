9c84f3431e3ece9dc91578a4fdca2803d32ec44b
Katie Lonsdale <katie952@gmail.com>
2023-06-13 10:20:46 -0600

    feat: add local_time method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -13,6 +13,11 @@ class ForecastFacade
     weather_service.hourly_weather.dig(:forecast, :forecastday, 0, :hour)
   end
 
+  def local_time
+    time = weather_service.local_time.dig(:location, :localtime)
+    DateTime.parse(time)
+  end
+
   private
 
   def location_service
